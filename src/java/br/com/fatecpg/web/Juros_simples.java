c0a157fcecb6ca005551de134e99d5bc1738df1c
mathsalgado <matheusms1212@gmail.com>
2019-08-19 17:13:06 -0300

    Merge origin/Branch1 into Branch1
    Conflicts:
    	src/java/br/com/fatecpg/web/Home.java

--- src/java/br/com/fatecpg/web/Juros_simples.java
+++ src/java/br/com/fatecpg/web/Juros_simples.java
@@ -43,14 +43,14 @@ public class Juros_simples extends HttpServlet {
             out.println("<h4> <a href='home.html'> Voltar </a> </h4>");
             out.println("<h2> Para calcularmos o Juros Simples necessitamos de: </h2>");
             out.println("<form action='resposta_simples.html'>"
-                    + "Valor do Capital:"
-                    + "<input type='text' name='c'/> </form> <br>");
-            out.println("<form action='resposta_simples.html'>"
-                    + "Taxa de juros:"
-                    + "<input type='text' name='i'/> </form> <br>");
-            out.println("<form action='resposta_simples.html'>"
-                    + "Período:"
-                    + "<input type='text' name='t'/> </form>");
+                    + "Valor do Capital: "
+                    + "<input type='text' name='c'/> <br>");
+            out.println("Taxa de juros: "
+                    + "<input type='text' name='i'/> <br>");
+            out.println("Período: "
+                    + "<input type='text' name='t'/> <br>");
+            out.println("<input type='submit' name 'montante' value='Gerar Montante'/>");
+            out.println("</form>");
             out.println("</body>");
             out.println("</html>");
         }
